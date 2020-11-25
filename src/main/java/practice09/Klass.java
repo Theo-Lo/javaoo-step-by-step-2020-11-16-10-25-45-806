f6ae4fd3e7e31e31bf6750cff46ced6d1add7796
Theo Lo <theo.lo@oocl.com>
2020-11-25 17:04:46 +0800

    [OOP][Theo] task9: Completed

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,37 @@
 package practice09;
 
 public class Klass {
+    private Integer number;
+    private Student leader;
+    private Student memeber;
+    private String displayName;
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student leader) {
+        if(leader.getKlass().getNumber() == number){
+            this.leader = leader;
+        }
+        else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student member) {
+        this.memeber = member;
+    }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,27 @@
 package practice09;
 
-public class Student {
+
+public class Student extends Person {
+    private Klass klass;
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if(klass.getLeader()==null){
+            return super.introduce()+" "+"I am a Student. I am at Class "+klass.getNumber()+".";
+        }
+        if(super.getName().equals(klass.getLeader().getName())){
+            return super.introduce()+" "+"I am a Student. I am Leader of Class "+klass.getNumber()+".";
+        }
+        return super.introduce()+" I am a Student. I am at Class "+klass.getNumber()+".";
+    }
 }
