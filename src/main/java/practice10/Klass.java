d109452551faea9aa6ec126c5e558df0b08f3f74
Theo Lo <theo.lo@oocl.com>
2020-11-25 17:33:01 +0800

    [OOP][Theo] task10: Completed

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,44 @@
 package practice10;
 
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+    private Student member;
+    private String displayName;
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student leader) {
+        if(leader.getKlass().getNumber() == number){
+            System.out.print("I am Tom. I know "+leader.getName()+" become Leader of "+leader.getKlass().getDisplayName()+".\n");
+            this.leader = leader;
+        }
+        else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        if(getLeader() == null){
+            System.out.print("I am Tom. I know "+student.getName()+" has joined "+getDisplayName()+".\n");
+        }else{
+            System.out.print("I am "+leader.getName()+". I know "+student.getName()+" has joined "+getDisplayName()+".\n");
+        }
+        student.getKlass().number = number;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,59 @@
 package practice10;
 
-public class Teacher {
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private Klass klass;
+    private List<Klass> classes;
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if(classes==null){
+            return getIntroduce()+"No Class.";
+        }
+        if(getClasses().size()!=0){
+            String teachingClasses = getClasses().stream()
+                    .map(Klass::getNumber)
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(", "));
+            return  getIntroduce() +"Class "+teachingClasses+".";
+        }
+        return getIntroduce()+"No Class.";
+    }
+
+    private String getIntroduce() {
+        return super.introduce()+" I am a Teacher. I teach ";
+    }
+
+    public String introduceWith(Student student) {
+        if(isTeaching(student)){
+            return  getIntroduce()+student.getName()+".";
+        }else{
+            return  super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+        }
+
+    }
+
+    public boolean isTeaching(Student student) {
+        boolean isInTeacherClasses = getClasses().stream()
+                .map(Klass::getNumber)
+                .anyMatch(classNumber->classNumber.equals(student.getKlass().getNumber()));
+        return isInTeacherClasses;
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
 }
